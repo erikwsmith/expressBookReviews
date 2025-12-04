9a4de0fe0fb97a4f8563d117491daa099da4fb86
erikwsmith <erikwsmith85@gmail.com>
2025-12-04 16:22:04 -0600

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,21 +11,31 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/',function (req, res) {  
+  return res.json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    let ISBN = req.params.isbn;
+    let book = books[ISBN];
+    if(book){
+        return res.json(book);
+    }else {
+        return res.send("ISBN not found.");
+    };
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+    let authorExists;
+
+    if(authorExists){
+        //return res.json();
+    }else {
+        //return res.send("Author not found.");
+    };
 });
 
 // Get all books based on title
